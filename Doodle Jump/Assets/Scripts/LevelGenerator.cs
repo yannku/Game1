1feadce6d1fd03e6bbdb298a326e0881d0d9499a
yannku <Yannick.Farrugia.a104041@mcast.edu.mt>
2018-04-04 00:21:35 +0200

    added level generator

---
+++ Doodle Jump/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+	//reference to the platform
+	public GameObject platformPrefab;
+	//no. of platforms that needs to be stored
+	public int numberOfPlatforms = 200;
+	//stores width of the level
+	public float levelWidth = 3f;
+	public float minY = .2f;
+	public float maxY = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+		Vector3 spawnPosition = new Vector3();
+		for (int i = 0; i < numberOfPlatforms; i++)
+		{
+			spawnPosition.y += Random.Range(minY, maxY);
+			spawnPosition.x = Random.Range(-levelWidth, levelWidth);
+			Instantiate(platformPrefab, spawnPosition, Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
